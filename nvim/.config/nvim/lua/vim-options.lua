f2dec28d785a1cbc72258e0752e7e93a7d18d4f2
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-01-22 11:05:09 -0300

    configurado clipboard para funcioar em ssh

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -16,17 +16,17 @@ vim.opt.number = true
 
 -- Clipboard Opts
 vim.opt.clipboard = "unnamedplus"
--- vim.g.clipboard = {
--- 	name = "OSC 52",
--- 	copy = {
--- 		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
--- 		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
--- 	},
--- 	paste = {
--- 		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
--- 		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
--- 	},
--- }
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Navigate to the left pane" })
