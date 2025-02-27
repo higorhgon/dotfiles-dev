f2ecf39114bc69a9c165cb00d41e63898caf6fdf
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-02-27 09:21:12 -0300

    atualizado blink cmp

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,255 +1,212 @@
 return {
-	{
-		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			-- "giuxtaposition/blink-cmp-copilot",
-			{ "L3MON4D3/LuaSnip", version = "v2.*" },
-		},
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        {
+            'L3MON4D3/LuaSnip',
+            version = 'v2.*',
+            config = function()
+                --- I had to add this option compared to the old syntax
+                require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+        },
+    },
 
-		-- use a release tag to download pre-built binaries
-		version = "*",
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = "cargo build --release",
+    -- use a release tag to download pre-built binaries
+    version = "*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = "cargo build --release",
 
-		opts = function(_, opts)
-			-- 'default' for mappings similar to built-in completion
-			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-			-- See the full "keymap" documentation for information on defining your own keymap.
-			opts.keymap = { preset = "default" }
+    opts = {
+        keymap = {
+            -- 'default' for mappings similar to built-in completion
+            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+            preset = 'default',
 
-			opts.appearance = {
-				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- Useful for when your theme doesn't support blink.cmp
-				-- Will be removed in a future release
-				use_nvim_cmp_as_default = true,
-				kind_icons = {
-					Copilot = " ",
-					Text = "󰉿 ",
-					Method = "󰊕 ",
-					Function = "󰊕 ",
-					Constructor = "󰒓 ",
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<C-Space>'] = {
+                function(cmp) cmp.show({ providers = { 'snippets' } }) end
+            },
 
-					Field = "󰜢 ",
-					Variable = "󰆦 ",
-					Property = "󰖷 ",
+            ['<A-1>'] = { function(cmp) cmp.accept({ index = 1 }) end },
+            ['<A-2>'] = { function(cmp) cmp.accept({ index = 2 }) end },
+            ['<A-3>'] = { function(cmp) cmp.accept({ index = 3 }) end },
+            ['<A-4>'] = { function(cmp) cmp.accept({ index = 4 }) end },
+            ['<A-5>'] = { function(cmp) cmp.accept({ index = 5 }) end },
+            ['<A-6>'] = { function(cmp) cmp.accept({ index = 6 }) end },
+            ['<A-7>'] = { function(cmp) cmp.accept({ index = 7 }) end },
+            ['<A-8>'] = { function(cmp) cmp.accept({ index = 8 }) end },
+            ['<A-9>'] = { function(cmp) cmp.accept({ index = 9 }) end },
+            ['<A-0>'] = { function(cmp) cmp.accept({ index = 10 }) end },
+        },
 
-					Class = "󱡠 ",
-					Interface = "󱡠 ",
-					Struct = "󱡠 ",
-					Module = "󰅩 ",
+        appearance = {
+            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+            -- Useful for when your theme doesn't support blink.cmp
+            -- Will be removed in a future release
+            use_nvim_cmp_as_default = true,
+            kind_icons = {
+                Copilot = ' ',
+                Text = '󰉿 ',
+                Method = '󰊕 ',
+                Function = '󰊕 ',
+                Constructor = '󰒓 ',
 
-					Unit = "󰪚 ",
-					Value = "󰦨 ",
-					Enum = "󰦨 ",
-					EnumMember = "󰦨 ",
+                Field = "󰜢 ",
+                Variable = "󰆦 ",
+                Property = "󰖷 ",
 
-					Keyword = "󰻾 ",
-					Constant = "󰏿 ",
+                Class = "󱡠 ",
+                Interface = "󱡠 ",
+                Struct = "󱡠 ",
+                Module = "󰅩 ",
 
-					Snippet = "󱄽 ",
-					Color = "󰏘 ",
-					File = "󰈔 ",
-					Reference = "󰬲 ",
-					Folder = "󰉋 ",
-					Event = "󱐋 ",
-					Operator = "󰪚 ",
-					TypeParameter = "󰬛 ",
-				},
-			}
+                Unit = "󰪚 ",
+                Value = "󰦨 ",
+                Enum = "󰦨 ",
+                EnumMember = "󰦨 ",
 
-			-- Default list of enabled providers defined so that you can extend it
-			-- elsewhere in your config, without redefining it, due to `opts_extend`
-			opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-				default = function(ctx)
-					local _ = ctx
-					local success, node = pcall(vim.treesitter.get_node)
-					if
-						success
-						and node
-						and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type())
-					then
-						return { "buffer" }
-					else
-						return {
-							"lsp",
-							"path",
-							"snippets",
-							"buffer",
-							-- "dadbod",
-							-- "copilot"
-						}
-					end
-				end,
-				per_filetype = {
-					lua = { "lsp", "path", "snippets", "buffer" },
-				},
-				providers = {
-					lsp = {
-						name = "LSP",
-						module = "blink.cmp.sources.lsp",
-						-- Filter text items from the LSP provider, since we have the buffer provider for that
-						transform_items = function(_, items)
-							for _, item in ipairs(items) do
-								if item.kind == require("blink.cmp.types").CompletionItemKind.Snippet then
-									item.score_offset = item.score_offset - 3
-								end
-							end
+                Keyword = "󰻾 ",
+                Constant = "󰏿 ",
 
-							return vim.tbl_filter(function(item)
-								return item.kind ~= require("blink.cmp.types").CompletionItemKind.Text
-							end, items)
-						end,
-						score_offset = 90,
-					},
-					path = {
-						name = "Path",
-						module = "blink.cmp.sources.path",
-						score_offset = 25,
-						-- When typing a path, I would get snippets and text in the
-						-- suggestions, I want those to show only if there are no path suggestions
-						fallbacks = { "snippets", "buffer" },
-						opts = {
-							trailing_slash = false,
-							label_trailing_slash = true,
-							get_cwd = function(context)
-								return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-							end,
-							show_hidden_files_by_default = true,
-						},
-					},
-					buffer = {
-						name = "Buffer",
-						enabled = true,
-						max_items = 3,
-						module = "blink.cmp.sources.buffer",
-						min_keyword_length = 4,
-						score_offset = 15,
-					},
-					snippets = {
-						name = "snippets",
-						max_items = 8,
-						min_keyword_length = 2,
-						module = "blink.cmp.sources.snippets",
-						score_offset = 85,
-						opts = {
-							friendly_snippets = true,
-							global_snippets = { "all" },
-							extended_filetypes = {
-								php = {
-									"html",
-									-- "css",
-									-- "javascript"
-								},
-							},
-							ignored_filetypes = {},
-							get_filetype = function(ctx)
-								local _ = ctx
-								return vim.bo.filetype
-							end,
-						},
-					},
-					-- dadbod = {
-					-- 	name = "Dadbod",
-					-- 	module = "vim_dadbod_completion.blink",
-					-- 	score_offset = 85,
-					-- },
-					-- copilot = {
-					-- 	name = "copilot",
-					-- 	module = "blink-cmp-copilot",
-					-- 	score_offset = 100,
-					-- 	async = true,
-					-- },
-				},
-				cmdline = function()
-					local type = vim.fn.getcmdtype()
-					if type == "/" or type == "?" then
-						return { "buffer" }
-					end
-					if type == ":" then
-						return { "cmdline" }
-					end
-					return {}
-				end,
-			})
+                Snippet = "󱄽 ",
+                Color = "󰏘 ",
+                File = "󰈔 ",
+                Reference = "󰬲 ",
+                Folder = "󰉋 ",
+                Event = "󱐋 ",
+                Operator = "󰪚 ",
+                TypeParameter = "󰬛 ",
+            },
+        },
 
-			opts.snippets = {
-				preset = "default",
-				-- Function to use when expanding LSP provided snippets
-				expand = function(snippet)
-					-- vim.snippet.expand(snippet)
-					require("luasnip").lsp_expand(snippet)
-				end,
-				-- Function to use when checking if a snippet is active
-				active = function(filter)
-					-- return vim.snippet.active(filter)
-					-- if filter and filter.direction then
-					-- 	return require("luasnip").jumpable(filter)
-					-- end
-					-- return require("luasnip").in_snippet()
-					return require("luasnip").jumpable(filter)
-				end,
-				-- Function to use when jumping between tab stops in a snippet, where direction can be negative or positive
-				jump = function(direction)
-					-- vim.snippet.jump(direction)
-					require("luasnip").jump(direction)
-				end,
-			}
+        sources = {
+            default = function(ctx)
+                local success, node = pcall(vim.treesitter.get_node)
+                if success and node and vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }, node:type()) then
+                    return { 'buffer' }
+                else
+                    return {
+                        'avante',
+                        'lsp',
+                        'path',
+                        'snippets',
+                        'buffer',
+                    }
+                end
+            end,
+            -- per_filetype = {
+            --     lua = { "lsp", "path", "snippets", "buffer" },
+            -- },
+            providers = {
+                path = {
+                    opts = {
+                        get_cwd = function(_)
+                            return vim.fn.getcwd()
+                        end,
+                        show_hidden_files_by_default = true,
+                    }
+                },
+                buffer = {
+                    opts = {
+                        get_bufnrs = function()
+                            return vim.tbl_filter(function(bufnr)
+                                return vim.bo[bufnr].buftype == ''
+                            end, vim.api.nvim_list_bufs())
+                        end
+                    }
+                },
+                snippets = {
+                    -- should_show_items = function(ctx)
+                    --     return ctx.trigger.initial_kind == 'trigger_character'
+                    -- end
+                    opts = {
+                        friendly_snippets = true,
+                        extended_filetypes = {
+                            php = {
+                                "html",
+                                -- "css",
+                                -- "javascript"
+                            },
+                        },
+                    },
+                },
+            }
+        },
 
-			-- Style
-			opts.completion = {
-				-- accept = {
-				-- 	auto_brackets = {
-				-- 		enabled = true,
-				-- 	},
-				-- },
-				menu = {
-					border = "rounded",
-					draw = {
-						treesitter = { "lsp" },
-						components = {
-							kind_icon = {
-								ellipsis = false,
-								text = function(ctx)
-									local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-									return kind_icon
-								end,
-								-- Optionally, you may also use the highlights from mini.icons
-								highlight = function(ctx)
-									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-									return hl
-								end,
-							},
-						},
-						columns = {
-							{ "label", "label_description", gap = 1 },
-							{ "kind_icon", "kind" },
-						},
-					},
-				},
-				-- Show documentation when selecting a completion item
-				documentation = {
-					window = {
-						border = "single",
-					},
-					auto_show = true,
-					auto_show_delay_ms = 500,
-				},
+        cmdline = {
+            sources = function()
+                local type = vim.fn.getcmdtype()
+                if type == "/" or type == "?" then
+                    return { "buffer" }
+                end
+                if type == ":" then
+                    return { "cmdline" }
+                end
+                return {}
+            end,
+        },
 
-				-- Display a preview of the selected item on the current line
-				-- ghost_text = { enabled = true },
-			}
+        snippets = {
+            preset = "default",
+            -- Function to use when expanding LSP provided snippets
+            expand = function(snippet) require("luasnip").lsp_expand(snippet) end,
+            -- Function to use when checking if a snippet is active
+            active = function(filter) return require("luasnip").jumpable(filter) end,
+            -- Function to use when jumping between tab stops in a snippet, where direction can be negative or positive
+            jump = function(direction) require("luasnip").jump(direction) end,
+        },
 
-			-- Experimental signature help support
-			opts.signature = {
-				enabled = true,
-				window = {
-					border = "rounded",
-				},
-			}
-		end,
-		opts_extend = { "sources.default" },
-	},
+        completion = {
+            -- auto_brackets = { enabled = true, },
+            menu = {
+                border = "rounded",
+                draw = {
+                    treesitter = { "lsp" },
+                    -- columns = { { 'item_idx' }, { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
+                    columns = {
+                        { 'item_idx' },
+                        { "label",     "label_description", gap = 1 },
+                        { "kind_icon", "kind",              gap = 1 },
+                    },
+                    components = {
+                        kind_icon = {
+                            ellipsis = false,
+                            text = function(ctx) return ctx.kind_icon .. ctx.icon_gap end,
+                        },
+                        item_idx = {
+                            text = function(ctx)
+                                return ctx.idx == 10 and '0' or ctx.idx >= 10 and ' ' or
+                                    tostring(ctx.idx)
+                            end,
+                            highlight = 'BlinkCmpItemIdx' -- optional, only if you want to change its color
+                        }
+                    },
+                },
+            },
+            documentation = {
+                window = {
+                    border = "single",
+                },
+                auto_show = true,
+                auto_show_delay_ms = 500,
+            },
+
+            -- Display a preview of the selected item on the current line
+            -- ghost_text = { enabled = true },
+        },
+
+        signature = {
+            enabled = true,
+            window = {
+                border = "rounded",
+            },
+        },
+    },
+    opts_extend = {
+        "sources.default"
+    }
 }
