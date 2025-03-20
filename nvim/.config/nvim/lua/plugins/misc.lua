a9b74a325168642fb9e32db175babc1c2da8eaa6
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-03-20 15:47:15 -0300

    sincronizado config do repositorio principal

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -18,9 +18,7 @@ return {
     },
     {
         "numToStr/Comment.nvim",
-        opts = {
-            -- add any options here
-        },
+        opts = {},
     },
     {
         "folke/persistence.nvim",
@@ -35,79 +33,73 @@ return {
             { "<leader>qd", ":lua require('persistence').stop()<CR>",                desc = "Don't Save Current Session" },
         },
     },
-    -- {
-    --     "mbbill/undotree",
-    --     keys = {
-    --         { "<leader>uU", ":UndotreeToggle<CR>:UndotreeFocus<CR>", desc = "Toggle Undotree" },
-    --     },
-    -- },
-	{
-		-- Useful plugin to show you pending keybinds.
-		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
-		opts = {
-			preset = "helix",
-			icons = {
-				-- set icon mappings to true if you have a Nerd Font
-				mappings = vim.g.have_nerd_font,
-				-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-				-- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-				keys = vim.g.have_nerd_font and {} or {
-					Up = "<Up> ",
-					Down = "<Down> ",
-					Left = "<Left> ",
-					Right = "<Right> ",
-					C = "<C-…> ",
-					M = "<M-…> ",
-					D = "<D-…> ",
-					S = "<S-…> ",
-					CR = "<CR> ",
-					Esc = "<Esc> ",
-					ScrollWheelDown = "<ScrollWheelDown> ",
-					ScrollWheelUp = "<ScrollWheelUp> ",
-					NL = "<NL> ",
-					BS = "<BS> ",
-					Space = "<Space> ",
-					Tab = "<Tab> ",
-					F1 = "<F1>",
-					F2 = "<F2>",
-					F3 = "<F3>",
-					F4 = "<F4>",
-					F5 = "<F5>",
-					F6 = "<F6>",
-					F7 = "<F7>",
-					F8 = "<F8>",
-					F9 = "<F9>",
-					F10 = "<F10>",
-					F11 = "<F11>",
-					F12 = "<F12>",
-				},
-			},
+    {
+        -- Useful plugin to show you pending keybinds.
+        "folke/which-key.nvim",
+        event = "VimEnter", -- Sets the loading event to 'VimEnter'
+        opts = {
+            preset = "helix",
+            icons = {
+                -- set icon mappings to true if you have a Nerd Font
+                mappings = vim.g.have_nerd_font,
+                -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+                -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+                keys = vim.g.have_nerd_font and {} or {
+                    Up = "<Up> ",
+                    Down = "<Down> ",
+                    Left = "<Left> ",
+                    Right = "<Right> ",
+                    C = "<C-…> ",
+                    M = "<M-…> ",
+                    D = "<D-…> ",
+                    S = "<S-…> ",
+                    CR = "<CR> ",
+                    Esc = "<Esc> ",
+                    ScrollWheelDown = "<ScrollWheelDown> ",
+                    ScrollWheelUp = "<ScrollWheelUp> ",
+                    NL = "<NL> ",
+                    BS = "<BS> ",
+                    Space = "<Space> ",
+                    Tab = "<Tab> ",
+                    F1 = "<F1>",
+                    F2 = "<F2>",
+                    F3 = "<F3>",
+                    F4 = "<F4>",
+                    F5 = "<F5>",
+                    F6 = "<F6>",
+                    F7 = "<F7>",
+                    F8 = "<F8>",
+                    F9 = "<F9>",
+                    F10 = "<F10>",
+                    F11 = "<F11>",
+                    F12 = "<F12>",
+                },
+            },
 
-			-- Document existing key chains
-			spec = {
-				{ "<leader>a", group = "[A]vante", mode = { "n" } },
-				{ "<leader>c", group = "[C]ode", mode = { "n", "x" } },
-				{ "<leader>d", group = "[D]ocument" },
-				{ "<leader>C", group = "[C]lipboard" },
-				{ "<leader>f", group = "[F]ind" },
-				{ "<leader>F", group = "[F]ormat" },
-				{ "<leader>g", group = "[G]it", mode = { "n", "v" } },
-				{ "<leader>m", group = "[M]ulti Cursor" },
-				{ "<leader>q", group = "[Q]Session", mode = { "n", "x" } },
-				{ "<leader>r", group = "[R]ename" },
-				{ "<leader>s", group = "[S]earch" },
-				{ "<leader>u", group = "[U]Toggle" },
-			},
-		},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
+            -- Document existing key chains
+            spec = {
+                { "<leader>a", group = "[A]vante",      mode = { "n" } },
+                { "<leader>c", group = "[C]ode",        mode = { "n", "x" } },
+                { "<leader>d", group = "[D]ocument" },
+                { "<leader>C", group = "[C]lipboard" },
+                { "<leader>f", group = "[F]ind" },
+                { "<leader>F", group = "[F]ormat" },
+                { "<leader>g", group = "[G]it",         mode = { "n", "v" } },
+                { "<leader>M", group = "[M]ulti Cursor" },
+                { "<leader>q", group = "[Q]Session",    mode = { "n", "x" } },
+                { "<leader>r", group = "[R]ename" },
+                { "<leader>s", group = "[S]earch" },
+                { "<leader>u", group = "[U]Toggle" },
+            },
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
 }
