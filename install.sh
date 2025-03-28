7a6387efc6c5fe041bf2df15c0be54168f29e308
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-03-28 09:05:49 -0300

    alterado build type para release

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ cd ~
 # Instala neovim
 git clone --recursive https://github.com/neovim/neovim.git
 cd neovim/
-make CMAKE_BUILD_TYPE=RelWithDebInfo
+make CMAKE_BUILD_TYPE=Release
 sudo make install
 
 cd ~
