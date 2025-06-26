864886cd1b9397cd70877c600346904fd5c42259
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-06-26 10:41:20 -0300

    teste

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,10 @@
 require("vim-options")
 require("config.lazy")
+
+-- Desabilitar modifyOtherKeys ao sair
+vim.api.nvim_create_autocmd("VimLeave", {
+	callback = function()
+		io.write("\027[>4;0m")
+		io.flush()
+	end,
+})
